fff4c466dabf26b7c8adcf0c731ae9ee5598e22f
Yingping Che <ypche29@gmail.com>
2017-09-19 23:54:18 +1000

    register the model: User, Message

--- Shutter/ShutterWeb/admin.py
+++ Shutter/ShutterWeb/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import User
+from .models import Message
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    class Meta:
+        model = User
+
+admin.site.register(User, UserAdmin)
+
+class MessageAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Message
+
+admin.site.register(Message, MessageAdmin)
