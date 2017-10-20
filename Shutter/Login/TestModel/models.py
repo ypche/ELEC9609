4d2dd6da8f569aa106b49e232f37df5a8d3ea64c
DIMES R F TU <517919664@qq.com>
2017-10-20 16:41:52 +1100

    delete outer Login Dir

--- Shutter/Login/TestModel/models.py
+++
@@ -1,9 +0,0 @@
-# models.py
-from django.db import models
- 
-class Test(models.Model):
-    name = models.CharField(max_length=20)
-
-class User(models.Model):
-    name = models.CharField(max_length=20)
-    password = models.CharField(max_length=6)
